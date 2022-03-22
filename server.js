3dd9f6f0292e2b0e12201ca0f020c79d80fd1dfe
Madyson Barber <madysonb@live.unc.edu>
2022-03-22 13:05:36 -0400

    help working

--- server.js
+++ server.js
@@ -7,8 +7,16 @@ args['port']
 args['help']
 args['debug']
 args['log']
+
 const HTTP_PORT = args.port
 const HELP = args.help
+const DEBUG = args.debug
+const LOG = args.log
+
+if (HELP) {
+    console.log('server.js [options]\n\n    --port	Set the port number for the server to listen on. Must be an integer\n                between 1 and 65535.\n\n    --debug	If set to `true`, creates endlpoints /app/log/access/ which returns\n                a JSON access log from the database and /app/error which throws \n                an error with the message "Error test successful." Defaults to \n                `false`.\n\n    --log	If set to false, no log files are written. Defaults to true.\n                Logs are always written to database.\n\n    --help	Return this message and exit.')
+    process.exit(0)
+}
 
 // Start an app server
 const server = app.listen(HTTP_PORT, () => {
