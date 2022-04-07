eb18d5279cc134a780b4194a83771211991f1ada
Madyson Barber <madysonb@live.unc.edu>
2022-04-07 14:29:20 -0400

    default params

--- server.js
+++ server.js
@@ -15,16 +15,19 @@ const db = new Database('log.db');
 
 
 // Require minimist for argument handling
-const args = require("minimist")(process.argv.slice(2))
+const args = require("minimist")(process.argv.slice(2), {
+    boolean: ['debug', 'log'],
+    default: { debug: false, log: true }
+})
 args['port']
 args['help']
 args['debug']
 args['log']
 
-const HTTP_PORT = args.port || 5555
+const HTTP_PORT = args.port || process.env.PORT || 5555
 const HELP = args.help
 const DEBUG = args.debug
-const LOG = args.log || true
+const LOG = args.log
 
 if (HELP) {
     console.log('server.js [options]\n\n    --port	Set the port number for the server to listen on. Must be an integer\n                between 1 and 65535.\n\n    --debug	If set to `true`, creates endlpoints /app/log/access/ which returns\n                a JSON access log from the database and /app/error which throws \n                an error with the message "Error test successful." Defaults to \n                `false`.\n\n    --log	If set to false, no log files are written. Defaults to true.\n                Logs are always written to database.\n\n    --help	Return this message and exit.')
