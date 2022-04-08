055c99354b6f7acec85de6c2b481b9bcc955f620
Madyson Barber <madysonb@live.unc.edu>
2022-04-07 21:25:14 -0400

    force id

--- server.js
+++ server.js
@@ -58,10 +58,11 @@ app.use((req, res, next) => {
     };
 
     const stmt = db.prepare(
-        "INSERT INTO accesslog (remoteaddr, remoteuser, time, method, url, protocol, httpversion, status, referer, useragent) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)"
+        "INSERT INTO accesslog (id, remoteaddr, remoteuser, time, method, url, protocol, httpversion, status, referer, useragent) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)"
     );
 
     const info = stmt.run(
+        1,
         logdata.remoteaddr,
         logdata.remoteuser,
         logdata.time,
